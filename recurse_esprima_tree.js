a41a686169da6829aaa5581e14e13c79c593caf9
cowemoji <krogank9@github.com>
2016-08-09 22:25:41 -0400

    esprima

--- recurse_esprima_tree.js
+++ recurse_esprima_tree.js
@@ -20,7 +20,7 @@ function traverseFileTree(item, path) {
 			var reader = new FileReader();
 			reader.onload = function(e) { // finished reading file data.
 				var tree = esprima.parse(e.target.result);
-				setTimeout(recurse_tree, 10, tree);
+				setTimeout(recurse_tree, 1*loading, tree);
 				//print_functions();
 			}
 			// start reading the file data.
