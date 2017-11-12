54f292ad760adf98d34a49c54ff515bc2bee45e6
a <a@a>
2017-11-12 00:57:04 -0500

    up

--- simple_SAT/phys.js
+++ simple_SAT/phys.js
@@ -2,23 +2,6 @@ canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
 sat_world = world();
 
-function drawAll() {
-	var ents = sat_world.ents;
-	for(var i=0; i<ents.length; i++) {
-		for(var j=0; j<ents.length; j++) {
-			if(i == j)
-				continue;
-			else if( ents[i].isOverlapping(ents[j]) ) {
-				ctx.fillStyle = "red";
-				break;
-			}
-			else
-				ctx.fillStyle = "black";
-		}
-		ents[i].draw(ctx);
-	}
-}
-
 // Create some boxes
 
 sat_world.createEnt( ent({ poly: poly.makeBox(200,100), x: 200, y: 190, rotDeg: -45, rotVelDeg: 20 }) );
@@ -35,6 +18,23 @@ var tri = [
 sat_world.createEnt( ent({ poly: tri, x: 400, y: 180, rotVelDeg: -10 }) );
 
 // Render loop
+
+function drawAll() {
+	var ents = sat_world.ents;
+	for(var i=0; i<ents.length; i++) {
+		for(var j=0; j<ents.length; j++) {
+			if(i == j)
+				continue;
+			else if( ents[i].isOverlapping(ents[j]) ) {
+				ctx.fillStyle = "red";
+				break;
+			}
+			else
+				ctx.fillStyle = "black";
+		}
+		ents[i].draw(ctx);
+	}
+}
 
 function step() {
 	window.requestAnimationFrame(step);
