3194b35ad2a00020c423a0d090479fce2644bc66
logan <a@a>
2017-11-21 16:04:25 -0500

    sat

--- simple_SAT/phys.js
+++ simple_SAT/phys.js
@@ -4,15 +4,18 @@ sat_world = new world();
 
 // Create some boxes and a triangle
 
-//sat_world.makeBox({ w:100, h:100, x: 110, y: -50, xVel: 0 });
+sat_world.makeBox({ w:100, h:100, x: 110, y: -50, xVel: 0 });
 
-sat_world.makeTriangle({b: 50, h: 100, x: 110, y: 100});
+sat_world.makeTriangle({b: 50, h: 100, x: 300, y: 100, rotDeg: 45});
 
-sat_world.makeBox({ w:100, h:100, x: 110, y: 264, xVel: 0 });
+sat_world.makeBox({ w:100, h:100, x: 230, y: 264, xVel: 0 });
 
-sat_world.makeBox({ w:100, h:130, x: 310, y: 264-15, xVel: 0});
+sat_world.makeBox({ w:100, h:130, x: 430, y: 264-15, xVel: 0});
 
+//walls
 sat_world.makeBox({ w:1000, h:100, x: 310, y: 500, xVel: 0, mass: 0 });
+sat_world.makeBox({ w:100, h:1000, x: -50, y: 250, xVel: 0, mass: 0 });
+sat_world.makeBox({ w:100, h:1000, x: 550, y: 250, xVel: 0, mass: 0 });
 
 // Render loop
 
