76e23ebccedb5e90b5f4d744a66b4ce4d637f3cf
krogank9 <12469653+krogank9@users.noreply.github.com>
2025-12-31 17:56:05 -0500

    up

--- phys-stuff/ezdraw.js
+++ phys-stuff/ezdraw.js
@@ -3790,8 +3790,7 @@ ez.callAnimate = function(callback, pauseOnHidden = true) {
             });
         }, {
             root: null,
-            threshold: 0,       // Trigger as soon as any pixel is visible/hidden
-            rootMargin: "100px" // Buffer zone to prevent early pausing on mobile
+            threshold: 0.1  // Start running when at least 10% visible
         });
 
         // Start observing the canvas
